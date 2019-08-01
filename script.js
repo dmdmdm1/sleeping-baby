cf3380c8ce16a6ac63ebbda6089a10160fb4b275
Dayana Mick <dayanamick@gmail.com>
2019-08-01 20:26:05 +0200

    two different kinds of pans falling

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ momImg.src = "./images/momavatar.png";
 const fryingPanImg = new Image();
 fryingPanImg.src = "./images/frying-pan.png";
 
+const bigPanImg = new Image();
+bigPanImg.src = "./images/big-pan.png";
+
+const panImages = [fryingPanImg, bigPanImg]
+
+//let randomPanImage = panImages[Math.floor(Math.random() * panImages.length)];
+
 let canvas;
 let ctx;
+let mom;
+let pan;
 let pansArray = [];
 let frameCounter = 0;
 
@@ -34,7 +43,7 @@ class Parent {
     }
   }
 
-  drawCharacter() {
+  draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 
@@ -55,7 +64,7 @@ class Pan extends SleepEnemy {
     super()
     this.image = image;
   }
-  drawPan() {
+  draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
@@ -77,20 +86,49 @@ class Pan extends SleepEnemy {
 
 
 function draw() {
-
-  ctx.fillStyle = "grey";
+  ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, 800, 800);
+  mom.draw();
+  pansArray.forEach((pan) => {
+    pan.draw()
+  })
+  if (frameCounter % 10 === 0) {
+    pansArray.forEach((pan) => {
+      pan.y += 10;
+    })
+  }
+  // every two seconds
+  if (frameCounter % 180 === 0) {
+    pansArray.push(new Pan(panImages[Math.floor(Math.random() * panImages.length)]));// add random here afterwards to randomize the image
+  }
 
+  frameCounter++;
+}
+
+function intersectParent(parent, object) {
+  let parentleft = parent.x;
+  let parenttop = parent.y;
+  let parentright = parent.x + parent.width;
+  let parentbottom = parent.y + parent.height;
+
+  let objectleft = object.x + 30;
+  let objecttop = object.y;
+  let objectright = object.x + object.width - 20;
+  let objectbottom = object.y + object.height;
+  return !(
+    parentleft > objectright ||
+    parentright < objectleft ||
+    parenttop > objectbottom ||
+    parentbottom < objecttop
+  );
 }
 
 window.onload = function () {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
-  draw()
-  let mom = new Parent(momImg, 100, 150, 350, 650)
-  mom.drawCharacter()
-  let pan1 = new Pan(fryingPanImg);
-  pan1.drawPan()
+  mom = new Parent(momImg, 100, 150, 350, 650)
+
+  setInterval(draw, 40)
 
   window.onkeydown = (event) => {
     if (event.keyCode === 39) {
@@ -99,12 +137,25 @@ window.onload = function () {
     } else if (event.keyCode === 37) {
       mom.moveLeft();
     }
-    draw()
-    mom.drawCharacter()
   }
+
+
 
 
 
   // declare a function in which the sleep objects start falling maybe make a subclass called pans and add
   // some different ones : pans and skillets , thenn implement the collision detection 
 }
+
+
+/* QUESTIONS
+
+Super w/o arguments
+appreciate `a code review, clean code and well organized
+Frame counter vs Request Animation Frame?
+Pan is added and now disappearing because canvas needs to get redrawn
+sparring partner to discuss data  structures and order of the code
+collision detection
+I have the parent that needs to be drawn and then the objects which position is being updated
+how do I organize this?
+*/
